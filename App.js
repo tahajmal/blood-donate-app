36155ad1898924b96b561d5817a67b4d6922012e
MUHAMMAD TAHA JAMAL <50657465+tahajmal@users.noreply.github.com>
2021-02-21 23:32:52 +0500

    done

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, Image, TouchableOpacity, TextInput, ScrollView } from 'react-native'
+
+import Navigation from './src/config/Navigation'
+
+const App = () => {
+  return (
+     <Navigation />
+  )
+}
+
+export default App
+
+
